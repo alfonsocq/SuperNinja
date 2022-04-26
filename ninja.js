bd2e02b7b453ddb3ac9ea724043a2843ade9367a
alfonsocq <alfcastroq@gmail.com>
2022-04-26 13:53:38 -0500

    masta

---
+++ ninja.js
@@ -0,0 +1,26 @@
+//PRACTICA
+class Ninja {
+    constructor(nombre, salud = 0, velocidad = 3, fuerza = 3) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.velocidad = velocidad;
+        this.fuerza = fuerza;
+    }
+    
+    sayName() {
+        console.log(this.nombre);
+    }
+
+    showStats() {
+        console.log("Nombre: "+this.nombre+ " Salud:"+this.salud+ " Velocidad: "+this.velocidad+ " Fuerza: " +this.fuerza);
+    }
+    
+    drinkSake() {
+        this.salud += 10;
+    }
+}
+
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
